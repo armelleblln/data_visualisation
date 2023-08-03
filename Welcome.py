b3d521ddbd7b5c75bc2665fd9c0855172a5c9766
armelleblln <141241914+armelleblln@users.noreply.github.com>
2023-08-03 09:51:44 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -19,4 +19,4 @@ el_list = df.columns.tolist()[27:80]
 st.selectbox('select element',[el_list])
 
 
-st.multiselect('select location', file_name_list)
+st.multiselect('select location', file_name_list,file_name_list[0])

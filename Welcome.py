91dbafaf7010975f4422af7af9259024d35542d9
armelleblln <141241914+armelleblln@users.noreply.github.com>
2023-08-03 10:46:33 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -24,13 +24,13 @@ st.selectbox('select element',[el_list])
 
 st.multiselect('select location', file_name_list,file_name_list[0]) #add the first location as a default option 
 
-x = range(1,14)
-y = [18.40, 20.92, 32.30, 32.03, 27.62, 27.77, 39.33, 21.48, 36.96, 35.78, 36.11, 31.10, 28.73, 29.40]
 
+x = [18.40, 20.92, 32.30, 32.03, 27.62, 27.77, 39.33, 21.48, 36.96, 35.78, 36.11, 31.10, 28.73, 29.40]
+y = range(1,14)
 p = figure(
     title='simple line example',
-    x_axis_label='Sample',
-    y_axis_label='Temperature (℃)')
+    x_axis_label='Temperature (\N{DEGREE SIGN}C)'
+    y_axis_label='Sample',)
 
 #p.line(x, y, legend_label='Trend', line_width=2)
 p.circle_dot(x, y, legend_label='Trend', size=14, color='navy', fill_color='orange', alpha = 0.5)

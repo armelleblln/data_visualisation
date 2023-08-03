5228f4b9f38cd46dd5bbfdd13d6bff8112c09de6
armelleblln <141241914+armelleblln@users.noreply.github.com>
2023-08-03 10:26:56 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -18,6 +18,12 @@ df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 
+el_list = df.columns.tolist()[27:80]
+st.selectbox('select element',[el_list])
+
+
+st.multiselect('select location', file_name_list,file_name_list[0]) #add the first location as a default option 
+
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
@@ -32,9 +38,3 @@ st.bokeh_chart(p, use_container_width=True)
 
 
 
-
-el_list = df.columns.tolist()[27:80]
-st.selectbox('select element',[el_list])
-
-
-st.multiselect('select location', file_name_list,file_name_list[0]) #add the first location as a default option 

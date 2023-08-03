36d461b28c95f50f207cde8cbd412c11611dc309
armelleblln <141241914+armelleblln@users.noreply.github.com>
2023-08-03 12:10:29 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -29,7 +29,7 @@ y = [18.40, 20.92, 32.30, 32.03, 27.62, 27.77, 39.33, 21.48, 36.96, 35.78, 36.11
 x = range(1,14)
 p = figure(
     title='Temperature in degree Celsius for the DVB Sample',
-    y_axis_label='Temperature ({DEGREE SIGN}C)',
+    y_axis_label='Temperature (\N{DEGREE SIGN}C)',
     x_axis_label='Sample')
 p.line(x, y, legend_label='Trend', color = 'orange', line_width=1, line_dash='dashed')
 p.circle_dot(x, y, size=14, color='navy', fill_color='orange', alpha = 0.5)
